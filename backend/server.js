d319ef8d43c2dfc507e1b5fd2f5a65c052b8489f
Sona Gupta <guptasona086@gmail.com>
2025-04-11 01:13:49 +0530

    URL Updated

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const __dirname = path.resolve();
 
 // Middlewares
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://reader-sbooknest.onrender.com", credentials: true }));
 app.use(express.json({ limit: "20mb" }));
 app.use(cookieParser());
 
